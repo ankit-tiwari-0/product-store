135674236273483ad172c51b0f0a9830508fc334
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-30 22:38:20 +0530

    post product api

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectdb from './db/db.js';
+import products from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/products", (req, res) =>{
-res.send("Sever is ready123")
+app.post("/api/products", async (req, res) =>{
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({success: false, message: "please provide all field"});
+  }
+
+  const newProduct = new products(product)
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct})
+  } catch (error) {
+    console.log("Error in create product:", error.message);
+    res.status(500).json({success: false, message: "server error"})
+  }
 });
+
+
 
 app.listen(5000, ()=> {
     connectdb();
